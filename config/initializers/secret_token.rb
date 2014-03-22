8fee8bb7d40e39a38697187030e3862fbcdca28b
Grant Van Sant <grantwenzlau@gmail.com>
2014-03-22 09:24:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taghash::Application.config.secret_key_base = '89f001619006d6f19d7b77476297323b3912a0303b9d8d730a983d91b4875832bfdf95fef07a200bec3385dd98c7aa739ed1ca92c01ee06b966488369310d8ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Taghash::Application.config.session_store :cookie_store, key: '_taghash_session'
