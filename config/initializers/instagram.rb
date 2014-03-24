902e4eec0fa8f2992b62f142e7ce29bd9ce2b956
Grant Van Sant <grantwenzlau@gmail.com>
2014-03-24 08:33:27 -0700

    inst.rb fix

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,11 +1,6 @@
 require "instagram"
 require 'json'
 
-baseurl = "https://api.instagram.com"
-path    = "/v1/tags/search"
-address = URI("#{baseurl}#{path}?#{query}")
-request = Net::HTTP::Get.new address.request_uri
-var query = $('#search').val();
 
     Instagram.configure do |config|
 
